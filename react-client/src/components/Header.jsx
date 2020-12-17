0546c7d4394674b4401cf6eee0f0372d8f9f7039
Tyna Nguyen <ngtyna@gmail.com>
2020-12-17 09:37:36 -0600

    Update Header component

--- react-client/src/components/Header.jsx
+++ react-client/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+// import AccountCircle from '@material-ui/icons/AccountCircle';
 
 function Header() {
   return (
     <Container className="header-container">
       <Row>
-        <Col sm={10}><p className="header-text">mvp-project</p></Col>
-        <Col sm={2}><p className="account-text">account-name</p></Col>
+        <Col sm={10} className="header-text-container"><p className="header-text">OKGO</p></Col>
+        <Col sm={2}>
+          <p className="account-text">Existing User</p>
+        </Col>
       </Row>
     </Container>
   );
