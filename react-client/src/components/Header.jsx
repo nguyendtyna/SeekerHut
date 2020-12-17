abc9712531c6d75ff877c115909b1eeedb907daf
Tyna Nguyen <ngtyna@gmail.com>
2020-12-16 21:37:22 -0600

    Change div containers to a React Container

--- react-client/src/components/Header.jsx
+++ react-client/src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Header() {
   return (
-    <div className="header-container">
+    <Container className="header-container">
       <Row>
         <Col sm={10}><p className="header-text">mvp-project</p></Col>
         <Col sm={2}><p className="account-text">account-name</p></Col>
       </Row>
-    </div>
+    </Container>
   );
 }
 
