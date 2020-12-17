c9a2f99695ca4da2a6e265735163724806813ad9
Tyna Nguyen <ngtyna@gmail.com>
2020-12-17 09:34:37 -0600

    Add additional webpack rules

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,18 @@ module.exports = {
         options: {
           'presets': ['@babel/preset-env', '@babel/preset-react']
         }
-      }
+      },
+      {
+				test: /\.css$/,
+				loader: 'style-loader!css-loader',
+			},
+			{
+				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+				},
+			},
     ]
   }
 };
